7bc110b275061bbdafe899db4450c7ac5f4b59cf
unknown <mdimranhossain.ist@gmail.com>
2023-12-04 14:08:52 +0600

    buildings rooms courses

--- src/app/modules/room/room.service.ts
+++ src/app/modules/room/room.service.ts
@@ -1,26 +1,30 @@
-import { Faculty, Prisma } from "@prisma/client";
-import { paginationHelpers } from "../../../helpers/paginationHelper";
-import { IGenericResponse } from "../../../interfaces/common";
-import { IPaginationOptions } from "../../../interfaces/pagination";
-import prisma from "../../../shared/prisma";
-import { facultyRelationalFields, facultyRelationalFieldsMapper, facultySearchableFields } from "./faculty.constants";
-import { IFacultyFilterRequest } from "./faculty.interface";
+import { Prisma, Room } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import {
+    roomRelationalFields,
+    roomRelationalFieldsMapper,
+    roomSearchableFields
+} from './room.constants';
+import { IRoomFilterRequest } from './room.interface';
 
-const insertIntoDB = async (data: Faculty): Promise<Faculty> => {
-    const result = await prisma.faculty.create({
+
+const insertIntoDB = async (data: Room): Promise<Room> => {
+    const result = await prisma.room.create({
         data,
         include: {
-            academicFaculty: true,
-            academicDepartment: true
+            building: true
         }
     });
     return result;
 };
 
 const getAllFromDB = async (
-    filters: IFacultyFilterRequest,
+    filters: IRoomFilterRequest,
     options: IPaginationOptions
-): Promise<IGenericResponse<Faculty[]>> => {
+): Promise<IGenericResponse<Room[]>> => {
     const { limit, page, skip } = paginationHelpers.calculatePagination(options);
     const { searchTerm, ...filterData } = filters;
 
@@ -28,7 +32,7 @@ const getAllFromDB = async (
 
     if (searchTerm) {
         andConditions.push({
-            OR: facultySearchableFields.map((field) => ({
+            OR: roomSearchableFields.map((field) => ({
                 [field]: {
                     contains: searchTerm,
                     mode: 'insensitive'
@@ -40,9 +44,9 @@ const getAllFromDB = async (
     if (Object.keys(filterData).length > 0) {
         andConditions.push({
             AND: Object.keys(filterData).map((key) => {
-                if (facultyRelationalFields.includes(key)) {
+                if (roomRelationalFields.includes(key)) {
                     return {
-                        [facultyRelationalFieldsMapper[key]]: {
+                        [roomRelationalFieldsMapper[key]]: {
                             id: (filterData as any)[key]
                         }
                     };
@@ -57,13 +61,12 @@ const getAllFromDB = async (
         });
     }
 
-    const whereConditions: Prisma.FacultyWhereInput =
+    const whereConditions: Prisma.RoomWhereInput =
         andConditions.length > 0 ? { AND: andConditions } : {};
 
-    const result = await prisma.faculty.findMany({
+    const result = await prisma.room.findMany({
         include: {
-            academicFaculty: true,
-            academicDepartment: true
+            building: true
         },
         where: whereConditions,
         skip,
@@ -75,7 +78,7 @@ const getAllFromDB = async (
                     createdAt: 'desc'
                 }
     });
-    const total = await prisma.faculty.count({
+    const total = await prisma.room.count({
         where: whereConditions
     });
 
@@ -89,48 +92,44 @@ const getAllFromDB = async (
     };
 };
 
-const getByIdFromDB = async (id: string): Promise<Faculty | null> => {
-    const result = await prisma.faculty.findUnique({
+const getByIdFromDB = async (id: string): Promise<Room | null> => {
+    const result = await prisma.room.findUnique({
         where: {
             id
         },
         include: {
-            academicFaculty: true,
-            academicDepartment: true
+            building: true
         }
     });
     return result;
 };
 
-const updateOneInDB = async (id: string, payload: Partial<Faculty>): Promise<Faculty> => {
-    const result = await prisma.faculty.update({
+const updateOneInDB = async (id: string, payload: Partial<Room>): Promise<Room> => {
+    const result = await prisma.room.update({
         where: {
             id
         },
         data: payload,
         include: {
-            academicFaculty: true,
-            academicDepartment: true
+            building: true
         }
     });
     return result;
 };
 
-const deleteByIdFromDB = async (id: string): Promise<Faculty> => {
-    const result = await prisma.faculty.delete({
+const deleteByIdFromDB = async (id: string): Promise<Room> => {
+    const result = await prisma.room.delete({
         where: {
             id
         },
         include: {
-            academicFaculty: true,
-            academicDepartment: true
+            building: true
         }
     });
     return result;
 };
 
-
-export const FacultyService = {
+export const RoomService = {
     insertIntoDB,
     getAllFromDB,
     getByIdFromDB,
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemeterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
+import { courseRoutes } from '../modules/course/course.routes';
 import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { roomRoutes } from '../modules/room/room.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 
 const router = express.Router();
@@ -10,24 +13,36 @@ const router = express.Router();
 const moduleRoutes = [
   // ... routes
   {
-    path: "/academic-semesters",
-    route: AcademicSemeterRoutes
+    path: '/academic-semesters',
+    route: AcademicSemeterRoutes,
   },
   {
     path: '/academic-faculties',
-    route: academicFacultyRoutes
+    route: academicFacultyRoutes,
   },
   {
     path: '/academic-departments',
-    route: academicDepartmentRoutes
+    route: academicDepartmentRoutes,
   },
   {
     path: '/faculties',
-    route: facultyRoutes
+    route: facultyRoutes,
   },
   {
     path: '/students',
-    route: studentRoutes
+    route: studentRoutes,
+  },
+  {
+    path: '/buildings',
+    route: buildingRoutes,
+  },
+  {
+    path: '/rooms',
+    route: roomRoutes,
+  },
+  {
+    path: '/courses',
+    route: courseRoutes,
   },
 ];
 
